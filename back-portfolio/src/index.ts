72eee89c3145a826d48adb6142026723fecd83b8
= <=>
2025-08-22 03:29:37 +0900

    change

--- back-portfolio/src/index.ts
+++ back-portfolio/src/index.ts
@@ -29,10 +29,10 @@ async function startServer() {
         });
 
         app.use(express.json());
-        app.use(express.static(path.join(__dirname, "../build")));
+        app.use(express.static(path.join(__dirname, "../front-portfolio/build")));
 
         app.get("*", (req, res) => {
-            res.sendFile(path.join(__dirname, 'build','index.html'));
+            res.sendFile(path.join(__dirname, 'index.html'));
         });
 
         app.post("/pet", async (req, res) => {
