e3a8958d92b2240fa7bf4523b3b3812f24604557
= <=>
2025-08-21 21:10:42 +0900

    index

--- back-portfolio/src/index.ts
+++ back-portfolio/src/index.ts
@@ -2,8 +2,12 @@ import express from "express";
 import { MongoClient } from "mongodb";
 import dotenv from "dotenv";
 import path from "path";
+import { fileURLToPath } from "url";
 
 dotenv.config();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const port = process.env.PORT || 5001;
